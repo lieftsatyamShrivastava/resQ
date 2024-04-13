27042b0a2d6d9e614afbd189360d900763e7fb7c
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-04-13 23:48:48 +0530

    updated contents

---
+++ src/app/dashboard/about/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+export default function page() {
+    return (
+
+
+        <div>page
+
+
+            <h2> this </h2>
+        </div>
+
+
+
+    )
+}
+
--- src/app/dashboard/doctor/page.tsx
+++ src/app/dashboard/doctor/page.tsx
@@ -1,8 +1,59 @@
 import React from 'react'
 
 const page = () => {
+
   return (
-    <div>page</div>
+    <div className='w-full p-10 bg-slate-900 h-[85vh] w-5/12'>
+      <div className='flex flex-col justify-end h-full bg-slate-950 w-full rounded-3xl p-5'>
+        <div className="chat chat-start">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </div>
+          <div className="chat-header">
+            Obi-Wan Kenobi
+            <time className="text-xs opacity-50">12:45</time>
+          </div>
+          <div className="chat-bubble">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">
+            Delivered
+          </div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </div>
+          <div className="chat-header">
+            Anakin
+            <time className="text-xs opacity-50">12:46</time>
+          </div>
+          <div className="chat-bubble">I hate you!</div>
+          <div className="chat-footer opacity-50">
+            Seen at 12:46
+          </div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </div>
+          <div className="chat-header">
+            <time className="text-xs opacity-50">12:46</time>
+          </div>
+          <div className="chat-bubble">I hate you!</div>
+          <div className="chat-footer opacity-50">
+            Seen at 12:46
+          </div>
+        </div>
+        <div className="w-full h-fit max-h-36 overflow-y-auto flex items-center justify-center p-3">
+        <textarea placeholder="Ask your Query..." className="p-6 py-3 textarea textarea-bordered textarea-xs w-full text-lg"></textarea>
+        </div>
+      </div>
+    </div>
   )
 }
 
