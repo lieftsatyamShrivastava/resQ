8127d96a190d9ca261d19d5a7fa10429e76db757
lieftsatyamsrivastava <satyamsrivastava384@gmail.com>
2024-04-13 17:58:40 +0530

    2 changes

---
+++ src/app/dashboard/ai/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function page ()  {
+  return (
+    <div>
+    <div className='bg-blue-300 flex justify-center'>
+
+<input type="text" placeholder="Type here" className="input input-bordered input-info w-full max-w-xs" />
+
+</div>
+
+    
+    </div>
+  )
+}
